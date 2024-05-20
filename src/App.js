3618bdc5c472cd73b62406a2ba2063d17ccd2015
Aswani Raj C k <aswanirajck04@gmail.com>
2024-05-20 21:51:22 +0530

    Removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import ProtectedRoute from './ProtectedRoute';
